32fe3ec523825f2c8858ac12464b4f3e113960e1
Gamz <jeydey@mail.ru>
2021-03-24 16:28:28 +0300

    debug 4

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 require("dotenv").config();
 const path = require("path");
-const logger = require("morgan");
+// const logger = require("morgan");
 const index = require("./routes/index");
 
 const app = express();
@@ -9,7 +9,7 @@ const app = express();
 app.set("views", path.join(process.env.PWD, "views"));
 app.set("view engine", "hbs");
 
-app.use(logger("dev"));
+// app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(process.env.PWD, "public")));
